ebda5aea7673a5d0956443b40ac7050c1a685390
edmond123456 <huajunjie123456@gmail.com>
2018-10-31 12:11:52 +0900

    video_fig.py added

--- toy/video_fig.py
+++ toy/video_fig.py
@@ -1,4 +1,6 @@
 ###   https://blog.csdn.net/huanglu_thu13/article/details/52337013
+###    https://blog.csdn.net/liuweiyuxiang/article/details/80591640
+
 
 import cv2  
 import numpy
@@ -9,6 +11,8 @@ cap = cv2.VideoCapture(0)
 while(1):
     # get a frame
     ret, frame = cap.read()
+    #rectangle
+    cv2.rectangle(frame,(0,0),(200,200),(0,255,0),3)
     # show a frame
     cv2.imshow("capture", frame)
     if cv2.waitKey(1) & 0xFF == ord('q'):
