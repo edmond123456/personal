2e6f2870ab90bac384f25e300eac585c6f5b47f1
edmond123456 <huajunjie123456@gmail.com>
2018-10-31 11:41:37 +0900

    video_test.py added

---
+++ toy/video_test.py
@@ -0,0 +1,18 @@
+###   https://blog.csdn.net/huanglu_thu13/article/details/52337013
+
+import cv2  
+import numpy
+import matplotlib.pyplot as plot
+
+cap = cv2.VideoCapture(0)
+
+while(1):
+    # get a frame
+    ret, frame = cap.read()
+    # show a frame
+    cv2.imshow("capture", frame)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+cap.release()
+cv2.destroyAllWindows() 
