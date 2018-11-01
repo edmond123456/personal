edbbe1e00accfe0e83b09153a7f2ad330c42edaf
edmond123456 <huajunjie123456@gmail.com>
2018-11-01 23:15:01 +0900

    1

---
+++ README.md
@@ -0,0 +1,7 @@
+# toy
+object recongnition based on AR
+
+
+
+#homepage
+[Junjie Hua](https://edmond123456.github.io/)
--- toy/video_fig.py
+++ toy/video_fig.py
@@ -1,5 +1,7 @@
 ###   https://blog.csdn.net/huanglu_thu13/article/details/52337013
 ###    https://blog.csdn.net/liuweiyuxiang/article/details/80591640
+### https://qiita.com/neriai/items/448a36992e308f4cabe2
+
 
 
 import cv2  
@@ -13,8 +15,26 @@ cap = cv2.VideoCapture(0)
 while(1):
     # get a frame
     ret, frame = cap.read()
-    #rectangle
-    cv2.rectangle(frame,(0,0),(200,200),(0,255,0),3)
+    # convert image to grayscale
+    gray_img = cv2.cvtColor(frame, cv2.COLOR_RGB2GRAY)
+    # threshold for image, with threshold 50. 超えなかった場合は、黒いピクセルに置き換える
+    _, threshold_img = cv2.threshold(gray_img, 50, 255, cv2.THRESH_BINARY)
+    _, contours, _= cv2.findContours(threshold_img,cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_NONE)
+    # 各輪郭に対する処理
+    for i in range(0, len(contours)):
+
+        # 輪郭の領域を計算
+        area = cv2.contourArea(contours[i])
+
+        # ノイズ（小さすぎる領域）と全体の輪郭（大きすぎる領域）を除外
+        if area < 1e3 or 1e5 < area:
+            continue
+
+        # 外接矩形
+        if len(contours[i]) > 0:
+            rect = contours[i]
+            x, y, w, h = cv2.boundingRect(rect)
+            cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 0, 255), 2)
     # show a frame
     cv2.imshow("capture", frame)
     if cv2.waitKey(1) & 0xFF == ord('q'):
