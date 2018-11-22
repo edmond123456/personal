0af1424b96b10350719a8c6abff0e5e4e250e620
Hua Junjie <junjie-hua@hiel.ist.osaka-u.ac.jp>
2018-11-22 10:08:53 +0900

    1

--- toy/quickstartup.bash
+++ toy/quickstartup.bash
@@ -1,1 +1,5 @@
 ###  /mnt/c/Users/saku_/Documents/GitHub/personal/toy
+
+###  python C:\Users\saku_\Documents\GitHub\personal\toy/quickstartup.bash
+
+python C:\Users\saku_\Documents\GitHub\personal\toy/video_audio_play.py
