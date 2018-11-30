c228c54d43bcb659de634984cb25a30c85b523fb
Hua Junjie <junjie-hua@hiel.ist.osaka-u.ac.jp>
2018-11-30 19:11:35 +0900

    1

--- toy/tishiyin.py
+++ toy/tishiyin.py
@@ -1,23 +1,31 @@
 import winsound
 import time
+import tkinter as tk 
 
 
-time.sleep(5)
-winsound.Beep(600,2000) # winsound.Beep(frequency, duration)
-print ("start")
-#time.sleep(2)
-#localtime = time.asctime( time.localtime(time.time()) )
-#print ("local time is :", localtime)
+file0 = "C:/Users/saku_/Documents/GitHub/personal/toy/oped.wav"
+file1 = "C:/Users/saku_/Documents/GitHub/personal/toy/new1.wav"
+file2 = "C:/Users/saku_/Documents/GitHub/personal/toy/new2.wav"
 
-for num in range(1,10):
-    winsound.Beep(1000,396)
-    #print ("on")
-    #time.sleep(2)
-    winsound.Beep(600,264)
-    #print ("off")
-    #time.sleep(2)
 
-#localtime = time.asctime( time.localtime(time.time()) )
-#print ("local time is :", localtime)
-winsound.Beep(600,2000)
-print ("end")
+
+root= tk.Tk() 
+   
+canvas1 = tk.Canvas(root, width = 300, height = 200) 
+canvas1.pack()
+
+def helloCallBack():
+    time.sleep(5)
+    winsound.PlaySound(file0,winsound.SND_FILENAME|winsound.SND_NOWAIT,)
+    for num in range(1,10):
+        winsound.PlaySound(file1,winsound.SND_FILENAME|winsound.SND_NOWAIT,)
+        winsound.PlaySound(file2,winsound.SND_FILENAME|winsound.SND_NOWAIT,)
+    winsound.PlaySound(file0,winsound.SND_FILENAME|winsound.SND_NOWAIT,)
+
+button1 = tk.Button (root, text='Start', command=helloCallBack) 
+canvas1.create_window(100, 100, window=button1) 
+      
+button2 = tk.Button (root, text='Exit', command=root.destroy) 
+canvas1.create_window(200, 100, window=button2) 
+    
+root.mainloop()
