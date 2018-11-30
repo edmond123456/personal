c30be36dfa309c6c29b9680b5c483bfb78778d5e
Hua Junjie <junjie-hua@hiel.ist.osaka-u.ac.jp>
2018-11-30 12:49:21 +0900

    提示音修改完成

--- toy/tishiyin.py
+++ toy/tishiyin.py
@@ -4,12 +4,20 @@ import time
 
 time.sleep(5)
 winsound.Beep(600,2000) # winsound.Beep(frequency, duration)
-time.sleep(2)
+print ("start")
+#time.sleep(2)
+#localtime = time.asctime( time.localtime(time.time()) )
+#print ("local time is :", localtime)
 
 for num in range(1,10):
-    winsound.Beep(600,2000)
-    time.sleep(2)
-    winsound.Beep(800,2000)
-    winsound.Beep(600,2000)
+    winsound.Beep(1000,396)
+    #print ("on")
+    #time.sleep(2)
+    winsound.Beep(600,264)
+    #print ("off")
+    #time.sleep(2)
 
-winsound.Beep(600,4000)
+#localtime = time.asctime( time.localtime(time.time()) )
+#print ("local time is :", localtime)
+winsound.Beep(600,2000)
+print ("end")
