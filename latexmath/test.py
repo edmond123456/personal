93f7713b73ec02f1aac105ce29d9eaf50e020b09
edmond123456 <junjie-hua@hiel.ist.osaka-u.ac.jp>
2019-03-16 02:35:58 +0900

    updated

--- latexmath/test.py
+++ latexmath/test.py
@@ -1,6 +1,19 @@
-"""  conda install -c anaconda sympy """
- 
-import sympy as sym
-import sympy.printing as printing
-string = sym.symbols('Delta__y_l')
-print(printing.latex(string))
+import matplotlib.pyplot as plt
+import numpy as np
+
+
+if __name__ == '__main__':
+    
+    x=np.arange(0.01,1,0.01)
+    y=0.5*np.log((1-x)/x)
+   
+    plt.grid()    
+    plt.subplots_adjust(top=0.9)
+    plt.scatter(x,y,label=r'$\alpha =\frac{1}{2}\ln(\frac{1-\varepsilon}{\varepsilon })$')
+    
+    plt.legend()
+    plt.xlabel(r'$\varepsilon$',fontsize=20)
+    plt.ylabel(r'$\alpha$',fontsize=20)
+    plt.xlim(0,1)    
+
+    plt.show()
