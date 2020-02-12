41a85808f698ed8e5cd7d8c5356496ccc1ae29d9
edmond123456 <huajunjie123456@gmail.com>
2020-02-12 15:31:01 +0900

    updated

---
+++ thermal_illusion/file_read.py
@@ -0,0 +1,34 @@
+import cv2
+import sys
+import os
+import numpy as np 
+from matplotlib import pyplot as plt 
+from matplotlib import cm
+
+img_path = 'C:/Users/saku_/Documents/GitHub/personal/thermal_illusion/'
+
+
+img_list = os.listdir(img_path)
+
+for img_name in img_list:
+    if '.tiff' in img_name:
+        print(img_name)
+        #保持不变读取
+        img = cv2.imread(os.path.join(img_path, img_name), -1)
+        print('non-transform reading')
+        print(img.dtype)
+        print(img.shape)
+        #默认读取方式
+        #img = cv2.imread(os.path.join(img_path, img_name))
+        #print('transform reading')
+        #print(img.dtype)
+        #print(img.shape)
+    
+"""
+figure read succeeded!
+"""
+
+img_trans = img/100 -273.15
+print(img_trans.shape)
+#plt.imshow(img_trans, cmap=cm.plasma,vmin=20,vmax=40) 
+#plt.show()
