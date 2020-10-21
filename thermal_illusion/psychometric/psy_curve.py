84094f564218518e811c02a4477b8e611a8d1266
edmond123456 <junjie-hua@hiel.ist.osaka-u.ac.jp>
2020-10-21 19:33:00 +0900

    updated

--- thermal_illusion/psychometric/psy_curve.py
+++ thermal_illusion/psychometric/psy_curve.py
@@ -1,6 +1,7 @@
 # https://psychology.stackexchange.com/questions/13347/how-can-i-fit-a-psychometric-function-such-that-the-minimum-is-50-chance-level
 # chance = 0.5  # between 0 and 1
 # y = chance + (1-chance) / (1 + np.exp(-k*(x-x0)))
+# plot standard deviation　https://stackoverflow.com/questions/22481854/plot-mean-and-standard-deviation
 
 
 import numpy as np
@@ -14,6 +15,7 @@ def sigmoid(x, x0, k):
 
 xdata = np.array([0.0,1.0,3.0,5.0,7.0,9.0])
 ydata = np.array([60,87.5,90,92.5,92.5,92.5])/100
+edata = np.array([22.36,12.99,7.07,8.29,8.29,8.29])/100
 
 popt, pcov = curve_fit(sigmoid, xdata, ydata)
 print (popt)
@@ -21,7 +23,9 @@ print (popt)
 x = np.linspace(0, 10, 50)
 y = sigmoid(x, *popt)*100 # percentage
 
-pylab.plot(xdata, ydata*100, 'o', label='data')
+#pylab.plot(xdata, ydata*100, 'o', label='data')
+pylab.errorbar(xdata,ydata*100,yerr=edata*100,fmt='o',ecolor='gray',color='black',elinewidth=1,capsize=4)
+
 pylab.plot(x,y, 'r',label='fit')
 pylab.xlim(0, 10)
 pylab.ylim(0, 100)
